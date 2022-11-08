19ac78a58f4647b2d9dae1d620f40c8cceeca11e
Arko <arkoroyb@gmail.com>
2022-11-08 21:03:12 +0600

    firebase authentication with email and password and update profile

---
+++ src/Context/AuthContext.js
@@ -0,0 +1,64 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile } from 'firebase/auth'
+import app from '../Firebase/firebase.config';
+
+
+export const AuthProvider = createContext();
+const auth = getAuth(app);
+
+
+const AuthContext = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const login = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // const googleSignIn = () => {
+    //     localStorage.removeItem('genius-token');
+    //     return signInWithPopup(auth, googleProvider);
+    // }
+
+    const updateUserProfile = (profile) => {
+        return updateProfile(auth.currentUser, profile);
+    }
+
+    const logOut = () => {
+        localStorage.removeItem('genius-token');
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            // console.log(currentUser);
+            setUser(currentUser);
+            setLoading(false)
+        })
+
+        return () => unsubscribe();
+    }, [])
+
+    const contextInfo = {
+        createUser,
+        login,
+        updateUserProfile,
+        logOut,
+
+    }
+
+    return (
+        <AuthProvider.Provider value={contextInfo}>
+            {children}
+        </AuthProvider.Provider>
+    );
+};
+
+export default AuthContext;
--- src/components/Pages/SignUp/SignUp.js
+++ src/components/Pages/SignUp/SignUp.js
@@ -1,9 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import signup from '../../../assets/23640-sign-in-or-sign-up-animation.gif'
+import { AuthProvider } from '../../../Context/AuthContext';
 
 
 const SignUp = () => {
+    const [errorFound, setErrorFound] = useState('');
+    const { createUser, logOut, updateUserProfile } = useContext(AuthProvider);
+
+
+    const navigate = useNavigate();
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setErrorFound('');
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photourl.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        // console.log(form);
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                // console.log(user);
+                form.reset();
+                handleUpdateUserProfile(name, photoURL);
+                logOut()
+                    .then(() => { })
+                    .catch(error => {
+                        setErrorFound(error.message)
+                        // notifyError()
+                    })
+                // setUser('')
+                // notify();
+                // verifyEmail();
+                navigate('/login');
+            })
+            .catch(error => {
+                console.error(error)
+                setErrorFound(error.message);
+                // notifyError();
+            })
+    }
+
+    const handleUpdateUserProfile = (name, photoURL) => {
+        setErrorFound('');
+        const profile = {
+            displayName: name,
+            photoURL: photoURL
+        }
+        updateUserProfile(profile)
+            .then(() => { })
+            .catch(error => {
+                setErrorFound(error.message)
+                // notifyError()
+            })
+    }
+
+
     return (
         <div className="hero bg-base-100 max-w-screen-xl mx-auto">
             <div className="hero-content flex-col lg:flex-row">
@@ -13,30 +70,30 @@ const SignUp = () => {
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                         <h4 className="text-2xl font-bold text-center">Sign Up</h4>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" placeholder="Name" className="input input-bordered" />
+                                <input type="text" placeholder="Name" name='name' className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="Email" className="input input-bordered" />
+                                <input type="email" placeholder="Email" name='email' className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Photo Url</span>
                                 </label>
-                                <input type="text" placeholder="Photo Url" className="input input-bordered" />
+                                <input type="text" name='photourl' placeholder="Photo Url" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="Password" className="input input-bordered" />
+                                <input type="password" name='password' placeholder="Password" className="input input-bordered" />
                                 <label className="label">
                                     <p>Already have an account? <Link to='/login'>Login</Link></p>
                                 </label>
